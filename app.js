f8e6ffe2266bfc55b0e875fd3194fcf4b16c4841
Ifraj-Crypto <ifrajahmed2003@gmail.com>
2025-10-06 13:10:39 +0600

    FInal Logic

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ let name_y = "";
 let form = document.querySelector("form")
 let alertBox = document.querySelector("#alertMessage")
 let gameContainer = document.querySelector("#game")
-let bug = document.querySelector("#bug")
+
+let x_stat = document.querySelector("#x_stat");
+let d_stat = document.querySelector("#d_stat");
+let o_stat = document.querySelector("#o_stat");
+
 
 const successMsgs = [
   "Game on! The duel of legends is about to begin ⚔️",
@@ -24,9 +28,9 @@ const handleFormSubmit = (e) => {
   e.preventDefault()
   let input_x = document.querySelector("#x").value;
   let input_y = document.querySelector("#y").value;
-  name_x=input_x;
-  name_y=input_y;
- 
+  name_x = input_x;
+  name_y = input_y;
+
 
   alertBox.innerText = "";
 
@@ -51,11 +55,19 @@ const handleFormSubmit = (e) => {
       gameContainer.classList.remove("hidden");
       setTimeout(() => {
         gameContainer.classList.remove("opacity-0");
-        
+
+        //GAME HEADERS
+        let countX = 0;
+        let countD = 0;
+        let countO = 0;
+
+        x_stat.innerHTML = `<span class="font-bold capitalize text-lg">${name_x} (X)</span><br>Victories: ${countX}`;
+        d_stat.innerHTML = `<span class="font-bold capitalize text-lg">Draws</span><br>${countD}`;
+        o_stat.innerHTML = `<span class="font-bold capitalize text-lg">${name_y} (O)</span><br>Victories: ${countO}`;
+
+
       }, 50);
     }, 2000);
-
-    //Remove the Form and Allow Game
 
 
   } else {
@@ -65,6 +77,9 @@ const handleFormSubmit = (e) => {
     p.innerText = failMsgs[Math.floor(Math.random() * failMsgs.length)];
     alertBox.appendChild(p);
   }
-  // console.log(`X is ${name_x} and Y is ${name_y}`)
+  console.log(`X is ${name_x} and Y is ${name_y}`)
 
 }
+
+//GAME
+
